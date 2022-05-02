42de0cbc3e4f4db21821dcfa909642724c4922a0
joeky72 <joey.conlan72@gmail.com>
2022-05-02 07:24:00 -0400

    Fixed crash errors. App works when published. Still waiting on casemarking folder write permissions.

--- AforgeWebcam1/Form1.cs
+++ AforgeWebcam1/Form1.cs
@@ -7,6 +7,7 @@ using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
@@ -58,29 +59,40 @@ namespace AforgeWebcam1
         private void button1_Click(object sender, EventArgs e)
         {
             path1 = @"\\leosv02ecc1.ec.local\userdata\Manufacturing\GSTest\DeptData\Conlan\";
-            path2 = @"\\leosv02ecc1.ec.local\userdata\Manufacturing\GSTest\CommonRW\case marking samples  2022\";
+            path2 = @"\\leosv02ecc1.ec.local\userdata\Manufacturing\GSTest\CommonRW\case marking samples  2022\"; 
             testpath = @"C:\images\";
             ndrive = "N:\\Manufacturing\\GSTest\\CommonRW\\case marking samples  2022";
             //path2 is the target destination. I don't have write privelidges here
 
             // The program will attempt to save the image as a .Jpeg to specified path then close the project
             // If an error occurs, the error will be displayed to the user then close the project.
-            try
+            if (videoCaptureDevice.IsRunning)
             {
-                Bitmap bm = new Bitmap(pic.Image);
-                bm.Save(path1 + LotID.Text + ".jpg", ImageFormat.Jpeg);
-                //Activate the below line of code only on computers with path2 write permission.
-                //It is most likely not needed, but is a possible workaround to a direct write.
-                System.IO.File.Move(path1 + LotID.Text + ".jpg", path2 + LotID.Text + ".jpg");
-                videoCaptureDevice.Stop();
-                MessageBox.Show(LotID.Text + " has been saved to " + ndrive);
-                Environment.Exit(0);
-            } 
-            catch (Exception error){
-                Console.WriteLine(error);
-                videoCaptureDevice.Stop();
-                MessageBox.Show(error.Message);
-                Environment.Exit(0);
+                Thread.Sleep(3000);
+                try
+                {
+                    
+                    Bitmap bm = new Bitmap(pic.Image);
+                    bm.Save(path2 + LotID.Text + ".jpg", ImageFormat.Jpeg);
+                    //Activate the below line of code only on computers with path2 write permission.
+                    //It is most likely not needed, but is a possible workaround to a direct write.
+                    //System.IO.File.Move(path1 + LotID.Text + ".jpg", path2 + LotID.Text + ".jpg");
+                    videoCaptureDevice.Stop();
+                    MessageBox.Show(LotID.Text + " has been saved to " + ndrive);
+                    Application.Exit();
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error);
+                    videoCaptureDevice.Stop();
+                    MessageBox.Show(error.Message);
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select Start Camera before you select save.");
+                Application.Exit();
             }
         }
     }
