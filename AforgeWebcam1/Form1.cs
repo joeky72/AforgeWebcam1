dc4227ab43b3aa7e2980e37ce59303722e0693b2
joeky72 <joey.conlan72@gmail.com>
2022-05-04 07:37:07 -0400

    Branch 1.0 updates for meeting with Kian

--- AforgeWebcam1/Form1.cs
+++ AforgeWebcam1/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 using System.Windows.Forms;
 using AForge.Video;
@@ -16,9 +10,9 @@ namespace AforgeWebcam1
 {
     public partial class Form1 : Form
     {
+        string testpath0;
+        string testpath1;
         string path1;
-        string path2;
-        string testpath;
         string ndrive;
         public Form1()
         {
@@ -58,25 +52,26 @@ namespace AforgeWebcam1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            path1 = @"\\leosv02ecc1.ec.local\userdata\Manufacturing\GSTest\DeptData\Conlan\";
-            path2 = @"\\leosv02ecc1.ec.local\userdata\Manufacturing\GSTest\CommonRW\case marking samples  2022\"; 
-            testpath = @"C:\images\";
+            testpath1 = @"\\leosv02ecc1.ec.local\userdata\Manufacturing\GSTest\DeptData\Conlan\";
+            path1 = @"\\leosv02ecc1.ec.local\userdata\Manufacturing\GSTest\CommonRW\case marking samples  2022\"; 
+            testpath0 = @"C:\images\";
             ndrive = "N:\\Manufacturing\\GSTest\\CommonRW\\case marking samples  2022";
-            //path2 is the target destination. I don't have write privelidges here
+            // path2 is the target destination. I don't have write privelidges here
 
             // The program will attempt to save the image as a .Jpeg to specified path then close the project
             // If an error occurs, the error will be displayed to the user then close the project.
             if (videoCaptureDevice.IsRunning)
             {
+                // Below line prevents crashes from camera not initializing before save is pressed
                 Thread.Sleep(3000);
                 try
                 {
                     
                     Bitmap bm = new Bitmap(pic.Image);
-                    bm.Save(path2 + LotID.Text + ".jpg", ImageFormat.Jpeg);
+                    bm.Save(testpath1 + LotID.Text + ".jpg", ImageFormat.Jpeg);
                     //Activate the below line of code only on computers with path2 write permission.
                     //It is most likely not needed, but is a possible workaround to a direct write.
-                    //System.IO.File.Move(path1 + LotID.Text + ".jpg", path2 + LotID.Text + ".jpg");
+                    //System.IO.File.Move(testpath1 + LotID.Text + ".jpg", path1 + LotID.Text + ".jpg");
                     videoCaptureDevice.Stop();
                     MessageBox.Show(LotID.Text + " has been saved to " + ndrive);
                     Application.Exit();
@@ -92,7 +87,7 @@ namespace AforgeWebcam1
             else
             {
                 MessageBox.Show("Please select Start Camera before you select save.");
-                Application.Exit();
+                Application.Restart();
             }
         }
     }
