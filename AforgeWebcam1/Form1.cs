b1052bd3929d8f43c39d4a98feb69caec9d6ed59
joeky72 <joey.conlan72@gmail.com>
2022-05-05 07:06:52 -0400

    FASTER

--- AforgeWebcam1/Form1.cs
+++ AforgeWebcam1/Form1.cs
@@ -63,7 +63,7 @@ namespace AforgeWebcam1
             if (videoCaptureDevice.IsRunning)
             {
                 // Below line prevents crashes from camera not initializing before save is pressed
-                Thread.Sleep(2000);
+                Thread.Sleep(1000);
                 try
                 {
                     
